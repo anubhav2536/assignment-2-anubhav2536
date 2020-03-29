637c4695429a749201dde2dbe7cacf69500492eb
anubhav2536 <anubhav.tomar_cs18@gla.ac.in>
2020-03-29 11:31:16 -0500

    final submission

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,35 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
-// executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
+/*
+ *  >  executable class
+ *  >  use problem1.mybst.MyBinarySearchTree class for creating binary tree
+ */
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        Scanner scanner = new Scanner(System.in);
+        /*
+         * > taking only 7 inputs for simple demonstration with value 1,2,3,4,5,6,7
+         */
+        int i = 7;
+        System.out.println("Please take only 7 inputs ...");
+        while (i > 0) {
+            try {
+                tree.add(scanner.nextInt());
+            } catch (Exception x) {
+                System.out.println("You Entered invalid no. ... you are kicked :) ");
+                break;
+            }
+            i--;
+        }
+        MyBinarySearchTree.preOrder(tree.root);
+        System.out.println();
+        tree.postOrder(tree.root);
+    }
 }
--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -5,6 +5,31 @@
  *  Time: 7:06 PM
  */
 package problem5.node;
-// to define node properties
+
+import problem5.student.Student;
+
+/*
+ *  to define node properties
+ */
 public class Node {
+    private Student student;
+    private Node next;
+
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
+
+    public Student getData() {
+        return student;
+    }
+
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
